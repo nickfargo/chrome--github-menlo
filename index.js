91e57e41c32dcb756a635c628b8b9db0b9af01b7
Nick Fargo <nick.fargo@gmail.com>
2013-04-06 02:57:09 -0700

    Add coverage; support gist

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var style = document.createElement('style');
-style.innerHTML = "pre,code,tt { font-family: Menlo, Consolas, Courier, monospace; }";
+style.innerHTML = "\
+pre, code, tt, .file-data pre, .file-data .line-numbers, .ace_editor,\
+input.url-field {\n\
+  font-family: Menlo, Consolas, Courier, monospace;\n\
+}\
+";
 document.querySelector('head').appendChild( style );
